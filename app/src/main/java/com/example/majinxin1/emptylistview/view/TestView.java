daa084628eeb482f3759f3d560047f74f21f1930
MAJINXIN1 <majinxin1@kingsoft.com>
2016-11-03 10:06:01 +0800

    新增TestView

---
+++ app/src/main/java/com/example/majinxin1/emptylistview/view/TestView.java
@@ -0,0 +1,19 @@
+package com.example.majinxin1.emptylistview.view;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ImageView;
+
+/**
+ * Created by MAJINXIN1 on 2016/11/3.
+ */
+
+public class TestView extends ImageView {
+    public TestView(Context context) {
+        super(context);
+    }
+
+    public TestView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
